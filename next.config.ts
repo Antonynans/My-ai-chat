66711e512d4a051a3f03ba3d71e1475a0c9ca012
antony <antonynans@gmail.com>
2026-04-05 16:13:00 +0100

    configured image hostname

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "firebasestorage.googleapis.com" },
+    ],
+  },
+  experimental: {
+    serverActions: { bodySizeLimit: "10mb" },
+  },
+  serverExternalPackages: ["firebase-admin"],
 };
 
 export default nextConfig;
